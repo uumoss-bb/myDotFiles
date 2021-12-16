4a37f0127c9df395ae44e4eab39c6ee3205f11c3
bb_personal_git <balserbrodie@gmail.com>
2021-12-16 10:30:59 -0700

    more

---
+++ brew.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install firefox
+brew cask install sublime-text
+brew cask install virtualbox
+brew cask install sourcetree
+brew cask install spotify
+brew cask install discord
+brew cask install google-backup-and-sync
+brew cask install skype
+brew cask install gimp
+brew cask install vlc
+brew cask install hyperdock
+brew cask install divvy
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew cask install font-source-code-pro
+
+
+awscli
+brotli
+c-ares
+gdbm
+gettext
+git
+httpie
+icu4c
+jemalloc
+libev
+libuv
+mongodb-community
+ncurses
+nghttp2
+node
+nvm
+openssl
+openssl@1.1
+pcre
+pcre2
+python
+readline
+redis
+sqlite
+xz
+zsh
